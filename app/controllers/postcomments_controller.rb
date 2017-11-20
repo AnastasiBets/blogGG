7750a0878b7d9d0f53e367dd68c7a7f7f130019e
ruby -v <anastasia.betsa@gmail.com>
2017-11-20 21:37:07 +0200

    lection 13/11

--- app/controllers/postcomments_controller.rb
+++ app/controllers/postcomments_controller.rb
@@ -1,26 +1,34 @@
 class PostcommentsController < ApplicationController
- before_action :authenticate_user!
- 
-   def create
-   	params[:postcomment][:post_id] = params[:post_id]
-   	params[:postcomment][:user_id] = current_user.id
-   	Postcomment.create(comment_params)
-   	redirect_to post_path(params[:post_id])
-   end
- 
-   def edit
-  	
-  	redirect_to post_path(params[:post_id])
+before_action :authenticate_user!
+
+  def create
+    params[:postcomment][:post_id] = params[:post_id]
+    params[:postcomment][:user_id] = current_user.id
+    @comment = Postcomment.create(comment_params)
+    if @comment.save
+      redirect_to post_path(params[:post_id])
+      else
+        @comment.errors.full_messages.each do |msg|
+          @msg = msg
+      end
+      flash[:notice] = "#{@msg}"
+      redirect_to post_path(params[:post_id])
+    end
+  end
+
+  def edit
+    
+    redirect_to post_path(params[:post_id])
   end
 
   def destroy
-  	
-  	redirect_to post_path(params[:post_id])
+    
+    redirect_to post_path(params[:post_id])
   end
 
- private
- 
- 	def comment_params
- 		params.require(:postcomment).permit(:post_id, :user_id, :comment)
- 	end
- end
+private
+
+  def comment_params
+    params.require(:postcomment).permit(:post_id, :user_id, :comment)
+  end
+end
